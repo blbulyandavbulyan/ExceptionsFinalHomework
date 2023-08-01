8d3c255297d771c943a400540ca5ec69a730d99d
David Blbulyan <blbulyandavbulyan@gmail.com>
2023-08-01 10:08:34 +0400

    added while and exit command

--- src/main/java/org/blbulyandavbulyan/calc/Main.java
+++ src/main/java/org/blbulyandavbulyan/calc/Main.java
@@ -8,13 +8,15 @@ public class Main {
     public static void main(String[] args) {
         ExpressionParser expressionParser = new ExpressionParser(new Calculator());
         Scanner sc = new Scanner(System.in);
-        try{
-            String expression = sc.nextLine();
-            System.out.println(expressionParser.parse(expression));
+        String expression;
+        while (!(expression = sc.nextLine()).equals("exit")) {
+            try {
+                System.out.println(expressionParser.parse(expression));
+            } catch (InvalidInputException e) {
+                System.out.println(e.getMessage());
+                if (e.hasCause()) System.out.println(e.getCause().getMessage());
+            }
         }
-        catch (InvalidInputException e){
-            System.out.println(e.getMessage());
-            if(e.hasCause())System.out.println(e.getCause().getMessage());
-        }
+
     }
 }
