7db403a1b34e0a3de1ccb8aa13dee386cba43c57
David Blbulyan <blbulyandavbulyan@gmail.com>
2023-08-01 09:57:49 +0400

    added hasCause method

--- src/main/java/org/blbulyandavbulyan/calc/exceptions/InvalidInputException.java
+++ src/main/java/org/blbulyandavbulyan/calc/exceptions/InvalidInputException.java
@@ -1,11 +1,17 @@
 package org.blbulyandavbulyan.calc.exceptions;
 
 public class InvalidInputException extends RuntimeException{
+    private boolean hasCause;
+
     public InvalidInputException(String message, Throwable cause) {
         super(message, cause);
+        if(cause != null)hasCause = true;
     }
 
     public InvalidInputException(String message) {
         super(message);
+    }
+    public boolean hasCause(){
+        return hasCause;
     }
 }
