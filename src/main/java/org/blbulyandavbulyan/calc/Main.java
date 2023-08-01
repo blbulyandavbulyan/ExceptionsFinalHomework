099ea01155243e19e9717cd1cd262226392a4039
David Blbulyan <blbulyandavbulyan@gmail.com>
2023-08-01 09:55:20 +0400

    added Main

--- src/main/java/org/blbulyandavbulyan/calc/Main.java
+++ src/main/java/org/blbulyandavbulyan/calc/Main.java
@@ -1,7 +1,20 @@
 package org.blbulyandavbulyan.calc;
 
+import org.blbulyandavbulyan.calc.exceptions.InvalidInputException;
+
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-
+        ExpressionParser expressionParser = new ExpressionParser(new Calculator());
+        Scanner sc = new Scanner(System.in);
+        try{
+            String expression = sc.nextLine();
+            System.out.println(expressionParser.parse(expression));
+        }
+        catch (InvalidInputException e){
+            System.out.println(e.getMessage());
+            System.out.println(e.getCause().getMessage());
+        }
     }
 }
