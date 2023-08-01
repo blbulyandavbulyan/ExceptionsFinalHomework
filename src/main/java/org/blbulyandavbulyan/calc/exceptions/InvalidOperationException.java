e2a00fc5784e7d2ac69339d65fe03c4d1c02271f
David Blbulyan <blbulyandavbulyan@gmail.com>
2023-08-01 09:52:41 +0400

    added InvalidOperationException

---
+++ src/main/java/org/blbulyandavbulyan/calc/exceptions/InvalidOperationException.java
@@ -0,0 +1,7 @@
+package org.blbulyandavbulyan.calc.exceptions;
+
+public class InvalidOperationException extends InvalidInputException{
+    public InvalidOperationException(String message) {
+        super(message);
+    }
+}
